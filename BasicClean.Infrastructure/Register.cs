4dbbe9af690babc8f8a862b595030f35add45e7a
fyzact <feyyazacet@gmail.com>
2020-11-07 09:52:10 +0300

    Add Entityframeworkcore Implemented efcoro

---
+++ BasicClean.Infrastructure/Register.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+namespace BasicClean.Infrastructure
+{
+    public static class Register
+    {
+        public static  IServiceCollection AddEntityFramework(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("Todo");
+            services.AddDbContextPool<TodoDbContext>(options => options.UseSqlServer(connectionString));
+            return services;
+        }
+    }
+}
---
+++ BasicClean.Infrastructure/TodoDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicClean.Infrastructure
+{
+     class TodoDbContext : DbContext
+    {
+        public TodoDbContext(DbContextOptions<TodoDbContext> options) : base(options)
+        { }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
