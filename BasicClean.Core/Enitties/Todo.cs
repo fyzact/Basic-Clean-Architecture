138dba608746368b3fa8e1eb6b18cb4a08becd5a
fyzact <feyyazacet@gmail.com>
2020-11-09 23:13:38 +0300

    Encapsulate Todo Create todo constructor signed private. define static Create method to initialize todo

--- BasicClean.Core/Enitties/Todo.cs
+++ BasicClean.Core/Enitties/Todo.cs
@@ -5,10 +5,28 @@ using System.Text;
 
 namespace BasicClean.Core.Enitties
 {
-   public class Todo:BaseEntity<int>
+    public class Todo : BaseEntity<Guid>
     {
+        private Todo()
+        {
+
+        }
         public string Title { get; set; }
         public string Content { get; set; }
         public TodoState State { get; set; }
+        public static Todo Create(string title, string content)
+        {
+            var todo = new Todo
+            {
+                Title = title,
+                Content = content,
+                State = TodoState.Created,
+                CreationDate=DateTime.Now,
+                IsDeleted=false,
+                Id=Guid.NewGuid()
+            };
+
+            return todo;
+        }
     }
 }
