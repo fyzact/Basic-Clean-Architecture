f55f93ed8b1d1f476cff73d8e61cfdecb04fdd35
fyzact <feyyazacet@gmail.com>
2020-11-16 22:13:54 +0300

    add argument controlTests when todo created wrote test to control title should not be null or empty

--- BasicClean.Core.Test/Entities/TodoFixture.cs
+++ BasicClean.Core.Test/Entities/TodoFixture.cs
@@ -10,7 +10,7 @@ namespace BasicClean.Core.Test.Entities
     public class TodoFixture
     {
         [Fact]
-        public void TodoId_ShouldNotBeNull_WhenCreated()
+        public void TodoId_ShouldNotBeEmpty_WhenCreated()
         {
             Todo todo = Todo.Create("title", "content");
             Assert.NotEqual(Guid.Empty, todo.Id);
@@ -21,6 +21,22 @@ namespace BasicClean.Core.Test.Entities
         {
             Todo todo = Todo.Create("title", "content");
             Assert.Equal(TodoState.Created, todo.State);
+        }
+
+        [Fact]
+        public void TodoTitle_ShouldBeNull_WhenCreated()
+        {
+            string title;
+            ArgumentException argument = Assert.Throws<ArgumentException>(()=> { Todo.Create(title, "content"); });
+            Assert.Equal($"{nameof(title)} cannot be null or empty", argument.Message);
+        }
+
+        [Fact]
+        public void TodoTitle_ShouldBeemty_WhenCreated()
+        {
+            string title=string.Empty;
+            ArgumentException argument = Assert.Throws<ArgumentException>(() => { Todo.Create(title, "content"); });
+            Assert.Equal($"{nameof(title)} cannot be null or empty", argument.Message);
         }
     }
 }
