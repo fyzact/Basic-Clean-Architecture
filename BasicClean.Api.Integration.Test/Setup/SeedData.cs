f1ca387829b021db10ffd1c9b02de5832ee43936
feyyazacet <feyyazacet@gmail.com>
2022-06-07 00:49:48 +0100

    update .net version as 6.0

--- BasicClean.Api.Integration.Test/Setup/SeedData.cs
+++ BasicClean.Api.Integration.Test/Setup/SeedData.cs
@@ -1,5 +1,6 @@
 ﻿using BasicClean.Core.Enitties;
 using BasicClean.Infrastructure;
+using System.Linq;
 
 namespace BasicClean.Api.Integration.Test.Setup
 {
@@ -10,8 +11,10 @@ namespace BasicClean.Api.Integration.Test.Setup
             var todo = Todo.Create("First title", "first content");
             //todo.ChangeStatus(Core.ValueObjects.TodoStatus.InProgress);
             todoDbContext.Todos.Add(todo);
-            todoDbContext.Todos.Add(Todo.Create("First title", "first content"));
+            var todo2 = Todo.Create("Second title", "second content");
+            todoDbContext.Todos.Add(todo2);
             todoDbContext.SaveChanges();
+            var lıst = todoDbContext.Todos.ToList();
         }
       
     }
--- BasicClean.Api/Startup.cs
+++ BasicClean.Api/Startup.cs
@@ -1,13 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using BasicClean.Core.Interfaces.Services;
 using BasicClean.Core.Services;
 using BasicClean.Infrastructure;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
